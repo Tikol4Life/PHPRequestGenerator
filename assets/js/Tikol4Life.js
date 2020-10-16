ff0c90ea30d518175c0790cd31ebcb5d744d4cf7
Tikol4Life <68922168+Tikol4Life@users.noreply.github.com>
2020-10-16 15:35:54 +0800

    replacing old files

--- assets/js/Tikol4Life.js
+++ assets/js/Tikol4Life.js
@@ -7,17 +7,13 @@ function generate(){
     var r_headers = $("#r_headers").val();
     var r_formdata = $("#r_formdata").val();
 
-    if (r_url.length == 0 || r_method.length == 0 || r_headers.length == 0 || r_formdata.length == 0) {
+    if (r_url.length == 0 || r_method.length == 0 || r_headers.length == 0) {
         $('#Modal').modal('show');
         $('#ModalTitle').text("REQ GENERATOR");
         $('#ModalMsg').text("Error: Missing fields detected.");
         playError();
         return;
     }
-
-    $("#r_results").text("<?php");
-
-
     $("#r_results").text("$ch = curl_init();\n");
     $("#r_results").append("curl_setopt($ch, CURLOPT_URL, '"+ r_url +"');\n");
     if(r_method == "POST"){
@@ -35,116 +31,22 @@ function generate(){
     $("#r_results").append("curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);\n");
     $("#r_results").append("curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);\n");
     $("#r_results").append("curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);\n");
-    $("#r_results").append("curl_setopt($ch, CURLOPT_POSTFIELDS, '"+ r_formdata +"');\n");
+    if (r_formdata.length != 0) {
+        $("#r_results").append("curl_setopt($ch, CURLOPT_POSTFIELDS, '"+ r_formdata +"');\n");
+    }
     $("#r_results").append("$curl = curl_exec($ch);\n");
     $("#r_results").append("curl_close($ch);\n");
     $("#r_results").append("echo $curl;");
     
+    var x = document.getElementById("r_results_id");
+    x.style.display = "block";
+    var y = document.getElementById("code");
+    y.style.display = "block";
 
 }
 
 
-function save_content_to_file(content, filename){
-    var dlg = false;
-    with(document){
-     ir=createElement('iframe');
-     ir.id='ifr';
-     ir.location='about.blank';
-     ir.style.display='none';
-     body.appendChild(ir);
-      with(getElementById('ifr').contentWindow.document){
-           open("text/plain", "replace");
-           charset = "utf-8";
-           write(content);
-           close();
-           document.charset = "utf-8";
-           dlg = execCommand('SaveAs', false, filename);
-       }
-       body.removeChild(ir);
-     }
-    return dlg;
-}
 
-
-function checkCards() {
-    var line = $("#cards").val();
-    var line = line.replace(/^\s*[\r\n]/gm, '');
-    $("#cards").val(line);
-    
-    var apis = $("#api").val();
-    var api_selected = $("#api option:selected").text();
-    var sk_live = $("#sk").val();
-
-    var tele_msg = $("#tele_msg").val();
-    var delay = $("#delay").val();
-
-    if (line.length == 0){
-        $('#Modal').modal('show');
-        $('#ModalTitle').text("CC Checker");
-        $('#ModalMsg').text("Error: Cards form empty.");
-        playError();
-        return;
-    }
-    if(api_selected.includes("need SK")){
-        if (sk_live.length == 0) {
-            $('#Modal').modal('show');
-            $('#ModalTitle').text("SK Checker");
-            $('#ModalMsg').text("Error: Secret Key (SK) form empty.");
-            playError();
-            return;
-        }
-        if (sk_live.indexOf('sk_live')==-1){
-            $('#Modal').modal('show');
-            $('#ModalTitle').text("SK Checker");
-            $('#ModalMsg').text("Error: Secret Key (SK) provided is invalid.");
-            playError();
-            return;
-        }
-    }
-    if(api_selected.includes("need SK")){
-        var sk_live = $("#sk").val();
-    }else{
-        var sk_live = "";
-    }
-
-    var telebot = $("#telebot").val();
-
-    if(telebot.length == 0) {
-        var telebot = "";
-    }
-
-	playClick();
-    var check_line = line.split("\n");
-    var total = check_line.length;
-    credits();
-    check_line.forEach(function(value, index) {
-        setTimeout(
-            function(){
-                $.ajax({
-                    url: apis + '?cc_info=' + value + '&sk=' + sk_live + '&referrer=Tikol4Life&telebot=' + telebot + "&tele_msg=" + tele_msg,
-                    type: 'GET',
-                    async: true,
-                    success: function(results) {
-                        var count_live_cvv = (eval(document.getElementById("approved_counter_cvv").innerHTML) + 1);
-                        var count_live_ccn = (eval(document.getElementById("approved_counter_ccn").innerHTML) + 1);
-                        var count_dead = (eval(document.getElementById("decline_counter").innerHTML) + 1);
-                        remove_line();
-                        if(results.match("CVV LIVE")) {
-                            $("#approved_counter_cvv").text(count_live_cvv);
-                            playSuccess();
-                        }else if(results.match("CCN LIVE")){
-                            $("#approved_counter_ccn").text(count_live_ccn);
-                            playSuccess();
-                        }else if(results.match("DEAD")){
-                            $("#decline_counter").text(count_dead);
-                        }
-                        result(results + "");
-                    }
-                });
-            }, delay * index
-        );
-    });
-}
 
 function credits(){
     if(!$("#footer").length){
